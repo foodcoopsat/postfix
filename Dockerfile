f5b1c5b567745d4c8691889916a5bbbd32debe6e
Patrick Gansterer <paroga@paroga.com>
2023-03-21 04:33:51 +0100

    Inital commit

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM alpine:3.17
+
+RUN apk add --no-cache bash postfix postfix-pcre postfix-pgsql rsyslog ruby ruby-json
+
+ENV MAIL_RECEIVER_VERSION=v4.0.7
+ENV MAIL_RECEIVER_SHA256=b460235340619973fda34bca4753a23f34d89d08420ebfc0923779b73158d3c7
+
+RUN apk add --no-cache curl \
+  && cd /tmp \
+  && rm -rf $INSTALL_PATH \
+  && curl -o mail-receiver.tar.gz -fSL https://github.com/discourse/mail-receiver/archive/${MAIL_RECEIVER_VERSION}.tar.gz \
+  && echo "$MAIL_RECEIVER_SHA256  mail-receiver.tar.gz" | sha256sum -c - \
+  && tar -xf mail-receiver.tar.gz \
+  && mkdir -p /usr/local/lib/site_ruby \
+  && mv mail-receiver-*/lib/mail_receiver /usr/local/lib/site_ruby \
+  && mv mail-receiver-*/receive-mail /usr/local/bin/discourse-receive-mail \
+  && sed -i 's/ENV_FILE, ARGV.first/ARGV[0], ARGV[1]/g' /usr/local/bin/discourse-receive-mail \
+  && rm -rf mail-receiver*
+
+COPY rsyslog.conf /etc/rsyslog.conf
+COPY start.sh /start.sh
+
+EXPOSE 25
+EXPOSE 465
+EXPOSE 587
+VOLUME /var/spool/postfix
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cp /etc/postfix/originals/* /etc/postfix/
+
+discourse_id=0
+while read base_url api_username api_key; do
+  cat << EOF > /etc/postfix/discourse_instance_$discourse_id.json
+{
+  "DISCOURSE_BASE_URL": "https://$base_url",
+  "DISCOURSE_API_USERNAME": "$api_username",
+  "DISCOURSE_API_KEY": "$api_key"
+}
+EOF
+  cat << EOF >> /etc/postfix/master.cf
+discourse_$discourse_id  unix  -       n       n       -       -       pipe user=nobody:nogroup
+  argv=/usr/local/bin/discourse-receive-mail /etc/postfix/discourse_instance_$discourse_id.json \${recipient}
+EOF
+  echo "$base_url discourse_$discourse_id:" >> /etc/postfix/transport.map
+  discourse_id=$((discourse_id+1))
+done < $DISCOURSE_INSTANCES_FILE
+
+relay_domains=""
+while read domain rest; do
+  relay_domains="${relay_domains},${domain}"
+  echo "noreply@${domain} 550 mailbox unavailable" >> /etc/postfix/recipient_list.map
+done < /etc/postfix/transport.map
+
+echo "relay_domains = ${relay_domains:1}" >> /etc/postfix/main.cf
+
+postmap /etc/postfix/*.map
+chown root:root /etc/postfix/*.db
+chmod 0600 /etc/postfix/*.db
+
+# Actually run Postfix
+/usr/libexec/postfix/master &
+rsyslogd -n
